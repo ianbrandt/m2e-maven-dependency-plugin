9a3bcbde45d5ae78c905c88f20dfeaad451349d2
Ian Brandt <ian@ianbrandt.com>
2012-07-15 10:36:17 -0700

    First draft implementation.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-M2E connector for Maven Dependency Plugin
-=========================================
+M2E connector for the Maven Dependency Plugin
+=============================================
 
-This m2e connector for the Maven Dependency Plugin will execute maven-dependency-plugin:unpack on Eclipse incremental
-builds.
+This m2e connector for the Maven Dependency Plugin adds a lifecycle mapping for the
+[unpack Mojo](http://maven.apache.org/plugins/maven-dependency-plugin/unpack-mojo.html).  It will be executed during
+incremental builds, and the configured outputDirectory will then be refreshed.
 
 Many thanks to [Fred Bricon](https://community.jboss.org/people/fbricon "Fred Bricon at JBoss") for his help on IRC;
 his work on [m2e](http://www.eclipse.org/m2e/) and [m2e-wtp](http://www.eclipse.org/m2e-wtp/); and his
-[m2e-wro4j connector](https://github.com/jbosstools/m2e-wro4j), which I cribbed from heavily to create this plugin.
+[m2e-wro4j connector](https://github.com/jbosstools/m2e-wro4j), which I cribbed from heavily to create this plug-in.
--- com.ianbrandt.m2e.mdp.core/src/com/ianbrandt/m2e/mdp/core/MdpBuildParticipant.java
+++ com.ianbrandt.m2e.mdp.core/src/com/ianbrandt/m2e/mdp/core/MdpBuildParticipant.java
@@ -1,20 +1,75 @@
 package com.ianbrandt.m2e.mdp.core;
 
+import java.io.File;
 import java.util.Set;
 
 import org.apache.maven.plugin.MojoExecution;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.embedder.IMaven;
+import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
+import org.eclipse.osgi.util.NLS;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
-public class MdpBuildParticipant extends AbstractBuildParticipant {
+public class MdpBuildParticipant extends MojoExecutionBuildParticipant {
+
+	private static final String OUTPUT_DIRECTORY = "outputDirectory";
 
 	public MdpBuildParticipant(MojoExecution execution) {
+
+		super(execution, true);
 	}
 
 	@Override
-	public Set<IProject> build(int arg0, IProgressMonitor arg1) throws Exception {
+	public Set<IProject> build(final int kind, final IProgressMonitor monitor) throws Exception {
 
-		return null;
+		final IMaven maven = MavenPlugin.getMaven();
+		final MojoExecution mojoExecution = getMojoExecution();
+		final BuildContext buildContext = getBuildContext();
+
+		if (mojoExecution == null) {
+			return null;
+		}
+
+		setTaskName(monitor);
+
+		final Set<IProject> result = executeMojo(kind, monitor);
+
+		final File outputDirectory = getOutputDirectory(maven, mojoExecution);
+
+		refreshOutputDirectory(buildContext, outputDirectory);
+
+		return result;
+	}
+
+	private File getOutputDirectory(final IMaven maven, final MojoExecution mojoExecution) throws CoreException {
+
+		return maven.getMojoParameterValue(getSession(), mojoExecution, OUTPUT_DIRECTORY, File.class);
+	}
+
+	private void setTaskName(IProgressMonitor monitor) {
+
+		if (monitor != null) {
+
+			final String taskName = NLS.bind("Invoking {0} on {1}", getMojoExecution().getMojoDescriptor().getFullGoalName(),
+					getMavenProjectFacade().getProject().getName());
+
+			monitor.setTaskName(taskName);
+		}
+	}
+
+	private Set<IProject> executeMojo(final int kind, final IProgressMonitor monitor) throws Exception {
+
+		return super.build(kind, monitor);
+	}
+
+	private void refreshOutputDirectory(final BuildContext buildContext, final File outputDirectory) {
+
+		if (outputDirectory != null && outputDirectory.exists()) {
+
+			buildContext.refresh(outputDirectory);
+		}
 	}
 }
