a20cd0443d14337c42d00c1060513d3a35a76c98
Ian Brandt <ian@ianbrandt.com>
2012-07-27 15:22:16 -0700

    Repackaged to com.ianbrandt.tools.m2e.mdp.  Simplified test execution.  Build stability improvements.

---
+++ tests/src/com/ianbrandt/tools/m2e/mdp/tests/M2eMdpConnectorTest.java
@@ -0,0 +1,26 @@
+package com.ianbrandt.tools.m2e.mdp.tests;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+@SuppressWarnings("restriction")
+public class M2eMdpConnectorTest extends AbstractMavenProjectTestCase {
+
+	public void testM2eMdpForSmoke() throws Exception {
+
+		IProject project = importProject("projects/smoketest/pom.xml");
+
+		waitForJobsToComplete();
+
+		project.build(IncrementalProjectBuilder.AUTO_BUILD, monitor);
+
+		waitForJobsToComplete();
+
+		IFile testResource = project
+				.getFile("target/generated-resources/plugin.xml");
+
+		assertTrue(testResource.getName() + " is missing", testResource.exists());
+	}
+}
